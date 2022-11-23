87ddace3cedc36b391301f036b02f8c43430a6de
Denis KOCAK <deniskocak37@gmail.com>
2022-11-23 20:26:04 +0100

    Adding 1-20 exercise (PFOUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU! i have to check why it is functional)

--- 1/20/main.c
+++ 1/20/main.c
@@ -1,53 +1,51 @@
 #include <stdio.h>
 
-#define COLONNES 4
 #define MAX_CHAR 1000
+#define COLONNES 8
 
-void vider(char ligne[], int limite);
 int lireligne(char ligne[], int limite);
+int tab_vers_esp(int i, int colonnes);
 
 int main()
 {
-    char ligne[MAX_CHAR];
-    vider(ligne, MAX_CHAR);
-    while (lireligne(ligne, MAX_CHAR) > 0)
-    {
-        for (int i = 0; ligne[i] != '\0'; ++i)
+        char ligne[MAX_CHAR];
+        int longueur;        
+        while ((longueur = lireligne(ligne, MAX_CHAR)) > 0)
         {
-            if (ligne[i] == '\t')
-            {
-                for (int j = 0; j < COLONNES; ++j)
-                    putchar(' ');
-            }
-            else
-                putchar(ligne[i]);
+                int i_tab = 0; // Index from last tab
+                for (int i = 0; i < longueur; ++i)
+                {
+                        if (ligne[i] == '\t')
+                        {
+                                for (int x = 0; x < tab_vers_esp(i - i_tab, COLONNES); ++x) // i - i_tab allows to process it like it was the first tab
+                                        putchar(' ');
+                                i_tab = i + 1; // We have to + 1 because i is not the length, just the index
+                        }
+                        else
+                                putchar(ligne[i]);
+                }
+                printf("||||%d\n", longueur);
         }
-        vider(ligne, MAX_CHAR);
-    }
-    return 0;
-}
 
-
-void vider(char ligne[], int limite)
-{
-    for (int i = 0; i < limite; ++i)
-        ligne[i] = 0;
+        return 0;
 }
 
 int lireligne(char ligne[], int limite)
 {
-    int i, c;
+        int c;
+        int i;
+        for (i = 0; i < (limite - 1) && (c = getchar()) != EOF && c != '\n'; ++i)
+                ligne[i] = c;
+        if (ligne[i] == '\n')
+        {
+                ligne[i] = '\n';
+                ++i;
+        }
+        ligne[i] = '\0';
+        return i;
+}
 
-    for (i = 0; i < (limite - 1) && (c = getchar()) != EOF && c != '\n'; ++i)
-    {
-        ligne[i] = c;
-    }
-    if (c == '\n')
-    {
-            ligne[i] = c;
-            ++i;
-    }
-    ligne[i] = '\0';
-    return i;
-
+int tab_vers_esp(int i, int colonnes)
+{
+        return colonnes - i % colonnes;
 }
